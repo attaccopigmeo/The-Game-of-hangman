140be8ec459b609557d12fa4683b0bd51f88730e
attaccopigmeo <camillaschmidt@yandex.ru>
2024-12-03 09:03:41 +0500

    hangman

--- Hangman.py
+++ Hangman.py
@@ -1,98 +1,103 @@
 import random
 
 
-def display_hangman(tries):
-    stages = [
-        """
-           -----
-           |   |
-           |   O
-           |  /|\
-           |  / \
-           -
-        """,
-        """
-           -----
-           |   |
-           |   O
-           |  /|\
-           |  /
-           -
-        """,
-        """
-           -----
-           |   |
-           |   O
-           |  /|
-           |  
-           -
-        """,
-        """
-           -----
-           |   |
-           |   O
-           |  
-           |  
-           -
-        """,
-        """
-           -----
-           |   |
-           |   
-           |  
-           |  
-           -
-        """,
-        """
-           -----
-           |   
-           |   
-           |  
-           |  
-           -
-        """,
+display_hangman = [
+    """
+       -----
+       |   |
+       |   O
+       |  /|\
+       |  / \
+       -
+    """,
+    """
+       -----
+       |   |
+       |   O
+       |  /|\
+       |  /
+       -
+    """,
+    """
+       -----
+       |   |
+       |   O
+       |  /|
+       |  
+       -
+    """,
+    """
+       -----
+       |   |
+       |   O
+       |  
+       |  
+       -
+    """,
+    """
+       -----
+       |   |
+       |   
+       |  
+       |  
+       -
+    """,
+    """
+       -----
+       |   
+       |   
+       |  
+       |  
+       -
+    """,
     ]
-    return stages[tries]
 
+WORDS = ['фонарь', 'аптека', 'репозиторий', 'лес', 'индексация', 'интерференция', '']
+def get_random_word():
+    """Выбирает случайное слово из списка."""
+    return random.choice(WORDS).lower()
 
-def choose_word():
-    words = ["идентификатор", "дерево", "друг", "интеллект", "умопомрачительный", "свеча", "огниво"]
-    return random.choice(words).upper()
+def display_board(hangman_pics, missed_letters, correct_letters, secret_word):
+    """Отображает текущее состояние игры."""
+    print(hangman_pics[len(missed_letters)])
+    print("\nОшибки:", " ".join(missed_letters))
+    blanks = ["_" if letter not in correct_letters else letter for letter in secret_word]
+    print("\nСлово:", " ".join(blanks))
 
+def main():
+    print("Добро пожаловать в игру 'Виселица'!")
+    secret_word = get_random_word()
+    missed_letters = []
+    correct_letters = []
+    game_over = False
 
-def game():
-    word = choose_word()
-    word_letters = set(word)
-    guessed_letters = set()
-    tries = 6
+    while not game_over:
+        display_board(display_hangman, missed_letters, correct_letters, secret_word)
+        guess = input("\nВведите букву: ").lower()
 
-    print('Игра "Виселица"')
-    print(display_hangman(tries))
-
-    while tries > 0 and word_letters:
-        word_display = [letter if letter in guessed_letters else '_' for letter in word]
-        print("Слово: " + ' '.join(word_display))
-        guess = input("Угадайте букву: ").upper()
         if len(guess) != 1 or not guess.isalpha():
-            print("Ошибка ввода. Вводите одну букву.")
+            print("Введите одну букву!")
             continue
-        if guess in guessed_letters:
-            print(f"Ужу угадано '{guess}'. Попробуйте снова.")
-            continue
-        guessed_letters.add(guess)
-        if guess in word_letters:
-            word_letters.remove(guess)
-            print(f"Вы угадали! '{guess}' есть в заданном слове.")
-        else:
-            tries -= 1
-            print(f"Неверно! '{guess}' нет в заданном слове. Попыток осталось: {tries}")
-        print(display_hangman(tries))
-    
-    if not word_letters:
-        print(f"Поздравляем Вас! Вы угадали слово: {word}")
-    else:
-        print(f"Вы проиграли! Заданное слово: {word}")
 
+        if guess in missed_letters + correct_letters:
+            print("Вы уже угадывали эту букву!")
+            continue
+
+        if guess in secret_word:
+            correct_letters.append(guess)
+            if all(letter in correct_letters for letter in secret_word):
+                display_board(display_hangman, missed_letters, correct_letters, secret_word)
+                print("\nПоздравляем! Вы угадали слово:", secret_word)
+                game_over = True
+        else:
+            missed_letters.append(guess)
+            if len(missed_letters) == len(display_hangman) - 1:
+                display_board(display_hangman, missed_letters, correct_letters, secret_word)
+                print("\nВы проиграли! Загаданное слово было:", secret_word)
+                game_over = True
+
+    if input("\nХотите сыграть снова? (да/нет): ").lower().startswith("д"):
+        main()
 
 if __name__ == "__main__":
-    game()
+    main()
